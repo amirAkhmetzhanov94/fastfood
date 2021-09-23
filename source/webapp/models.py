5aae94d702247a394564c690024212d39e069c22
Akhmetzhanov_Amir <amir.akhmetzhanov94@gmail.com>
2021-09-23 22:08:13 +0600

    Немного подправил модель проекта

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,24 +1,19 @@
-from typing import Text
 from django.db import models
 from django.db.models.fields import BooleanField, DecimalField
-from django.db.models.fields.related import ForeignKey
 
-class Category:
-    category = models.CharField(max_length=20)
-    
-    def __str__(self):
-        return self.category
+CATEGORY_CHOICES = ['Напиток', 'Еда']
 
 
-class Dish: 
+class Dish(models.Model):
     title = models.CharField(max_length=50)
-    category = ForeignKey("webapp.Category", verbose_name='category', on_delete=models.PROTECT)
+    category = models.TextField(max_length=15, null=False, blank=False, verbose_name="Category",
+                                choices=CATEGORY_CHOICES)
     price = DecimalField(max_digits=10, decimal_places=2)
     activated = BooleanField(default=False)
 
 
-class Order:
-    dish_in_order = models.CharField(max_length=100)  #немного не понял что с ней делать
+class Order(models.Model):
+    dish_in_order = models.ForeignKey('webapp.Dish', on_delete=models.PROTECT, verbose_name='Dish in order',
+                                      related_name='orders')  # немного не понял что с ней делать
     completed_order = BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-
