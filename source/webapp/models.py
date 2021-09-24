e94d3d4aba0a5047aecf9fe9e740a7aa579b5068
Akhmetzhanov_Amir <amir.akhmetzhanov94@gmail.com>
2021-09-24 12:48:39 +0600

    Обновил отображение объектов моделей в админке

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -11,9 +11,15 @@ class Dish(models.Model):
     price = DecimalField(max_digits=10, decimal_places=2)
     activated = BooleanField(default=False)
 
+    def __str__(self):
+        return f'{self.title} ({self.category})'
+
 
 class Order(models.Model):
     dish_in_order = models.ForeignKey('webapp.Dish', on_delete=models.PROTECT, verbose_name='Dish in order',
                                       related_name='orders')  # немного не понял что с ней делать
     completed_order = BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.dish_in_order} (Completed Order: {self.completed_order})"
