cf9e693d9986447b42007a6296ff7924601a286f
Akhmetzhanov_Amir <amir.akhmetzhanov94@gmail.com>
2021-09-24 22:10:00 +0600

    Добавил вывод сообщения в случае отсутствия товаров в корзине

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View, TemplateView
 from webapp.models import Dish, Order
+from django.db.models import Q
 
 
 class Index(TemplateView):
     template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
-        extra_context = {'orders': Order.objects.all()}
+        extra_context = {'dishes': Dish.objects.all(), 'number_of_dishes': 0}
+        if Order.objects.all():
+            extra_context['orders'] = Order.objects.all()
+            extra_context['number_of_dishes'] = Order.objects.count()
         return extra_context
