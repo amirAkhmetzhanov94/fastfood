817696d2c7c193e30cc294693fec2ac7e2823744
Akhmetzhanov_Amir <amir.akhmetzhanov94@gmail.com>
2021-09-25 13:56:49 +0600

    Написал логику вывода количества товара, его цены и общей суммы в чеке и добавил её на главную страницу

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View, TemplateView
 from webapp.models import Dish, Order
 from django.db.models import Q
+from django.db.models import Count
 
 
 class Index(TemplateView):
@@ -10,8 +11,13 @@ class Index(TemplateView):
     def get_context_data(self, **kwargs):
         extra_context = {'dishes': Dish.objects.all(), 'number_of_dishes': 0}
         if Order.objects.all():
-            extra_context['orders'] = Order.objects.all()
             extra_context['number_of_dishes'] = Order.objects.count()
+            dishes_count = Order.objects.values('dish_in_order__title', 'dish_in_order__price').annotate(
+                count=Count('dish_in_order__title'))
+            extra_context['total_price'] = 0
+            for total_price in dishes_count:
+                extra_context['total_price'] += (total_price["dish_in_order__price"] * total_price["count"])
+            extra_context['dishes_count'] = dishes_count
         return extra_context
 
 
