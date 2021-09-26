b8365999c5d88393dc741fb3902c9dabd292a9fc
Akhmetzhanov_Amir <amir.akhmetzhanov94@gmail.com>
2021-09-27 00:25:42 +0600

    Обновил модель: 'добавил вручную OrderDish таблицу со значением 'qty''

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from webapp.models import Dish, Order
+from webapp.models import Dish, Order, OrderDish
 
 admin.site.register(Dish)
 admin.site.register(Order)
+admin.site.register(OrderDish)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -19,9 +19,22 @@ class Dish(models.Model):
         verbose_name = "dish"
 
 
+class OrderDish(models.Model):
+    order_id = models.ForeignKey('webapp.Order', related_name='order_dishes', on_delete=models.CASCADE,
+                                 verbose_name='Order')
+    dish_id = models.ForeignKey('webapp.Dish', related_name='dish_orders', on_delete=models.CASCADE,
+                                verbose_name='Dish')
+    qty = models.PositiveIntegerField()
+
+    class Meta:
+        verbose_name_plural = "Order dishes"
+
+
 class Order(models.Model):
-    dish = models.ManyToManyField('webapp.Dish', verbose_name='Dish in order',
-                                  related_name='orders')
+    dishes = models.ManyToManyField('webapp.Dish', verbose_name='Dish in order',
+                                    related_name='orders', through='webapp.OrderDish', through_fields=('order_id',
+                                                                                                       'dish_id'),
+                                    blank=True)
     completed_order = BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
