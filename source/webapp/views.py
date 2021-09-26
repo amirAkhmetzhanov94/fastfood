b2a00b6bb5b853c08f942be449c7b3c06beb309d
Akhmetzhanov_Amir <amir.akhmetzhanov94@gmail.com>
2021-09-26 11:06:13 +0600

    Переписал вывод заказа на главной странице. Добавил кнопку добавления блюда в заказ. Переписал метод добавления блюда в заказ в соответствии с новой моделью.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -5,20 +5,32 @@ from django.db.models import Q
 from django.db.models import Count
 
 
-class Index(TemplateView):
+class Index(View):
     template_name = 'index.html'
 
-    def get_context_data(self, **kwargs):
-        extra_context = {'dishes': Dish.objects.all(), 'number_of_dishes': 0}
-        if Order.objects.all():
-            extra_context['number_of_dishes'] = Order.objects.count()
-            dishes_count = Order.objects.values('dish_in_order__title', 'dish_in_order__price').annotate(
-                count=Count('dish_in_order__title'))
-            extra_context['total_price'] = 0
-            for total_price in dishes_count:
-                extra_context['total_price'] += (total_price["dish_in_order__price"] * total_price["count"])
-            extra_context['dishes_count'] = dishes_count
-        return extra_context
+    def get(self, request, *args, **kwargs):
+        dishes = Dish.objects.all()
+        return render(request, self.template_name, {"dishes": dishes})
+
+    def post(self, request, *args, **kwargs):
+        dishes = Dish.objects.all()
+        order = Order.objects.create()
+        dish_id = Dish.objects.filter(title=request.POST["dish__title"]).values("id").first()
+        order.dish.set(f"{dish_id['id']}")
+        return render(request, self.template_name, {'dishes': dishes})
+    # def get_context_data(self, **kwargs):
+    #     extra_context = {'dishes': Dish.objects.all(), 'number_of_dishes': 0}
+    #     Order.objects.values("id")
+    #     if Order.objects.all():
+    #         extra_context['number_of_dishes'] = Order.objects.count()
+    #         extra_context['orders'] = Order.objects.values("id")
+    #         # dishes_count = Order.objects.values('dish_in_order__title', 'dish_in_order__price').annotate(
+    #         #     count=Count('dish_in_order__title'))
+    #         extra_context['total_price'] = 0
+    #         # for total_price in dishes_count:
+    #         #     extra_context['total_price'] += (total_price["dish_in_order__price"] * total_price["count"])
+    #         # extra_context['dishes_count'] = dishes_count
+    #     return extra_context
 
 
 class OrdersView(TemplateView):
